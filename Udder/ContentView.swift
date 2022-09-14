ba4bed7016b665dd82df3072b110b852b2fc7e68
Artem <moiseev@physics.msu.ru>
2022-09-14 22:20:17 +0300

    m,m,m,,m

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, velvet world!")
             .padding()
-        Text ("Dudcan zhorzhperjan")
+        Text ("Dudcan zhorzhperjan koolll;")
     }
 }
 
